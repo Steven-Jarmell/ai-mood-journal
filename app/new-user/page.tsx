82764c4a2813223ba28c8c0038c35e3a49c03361
Steven-Jarmell <sjj27@pitt.edu>
2023-09-11 16:50:52 -0400

    Add prisma and routes for journals

--- app/new-user/page.tsx
+++ app/new-user/page.tsx
@@ -1,5 +1,34 @@
-const NewUser = () => {
-    return <div>New User Page</div>
+import { prisma } from "@/utils/db"
+import { currentUser } from "@clerk/nextjs"
+import { redirect } from "next/navigation"
+
+const createNewUser = async () => {
+    // Should never be null since you have to be authenticated to reach this page
+    const user = await currentUser()
+
+    const match = await prisma.user.findUnique({
+        where: {
+            clerkId: user.id as string,
+        }
+    })
+
+    // If there was no match, this is a brand new user
+    if (!match) {
+        await prisma.user.create({
+            data: {
+                clerkId: user.id,
+                email: user?.emailAddresses[0].emailAddress
+            }
+        })
+    }
+
+    redirect('/journal')
+}
+
+// Essentially is just a route
+const NewUser = async () => {
+    await createNewUser()
+    return <div>...loading</div>
 }
 
 export default NewUser
